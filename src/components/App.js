2dd3e6a896b8fe3eb174bf542858a0eba51ec064
warishshahbaz <mdshahbazwarish1996@gmail.com>
2024-04-07 16:08:11 +0530

    impl

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,24 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+{
+  /* <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p> */
+}
+import React, { useState } from "react";
+import "../styles/App.css";
 
+function App() {
+  const [input, setInput] = useState("");
+  return (
+    <div>
+      <main className="app">
+        <input
+          type="textarea"
+          onChange={(e) => setInput(e.target.value)}
+          className="textarea"
+          placeholder="Text Area"
+        />
+        <section className="preview">{input}</section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
