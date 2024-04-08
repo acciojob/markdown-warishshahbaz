d6704d58550b26fdf359064d94914eaca220af2b
warishshahbaz <mdshahbazwarish1996@gmail.com>
2024-04-08 12:51:29 +0530

    enhanced

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,29 @@
-import React, { useState, useEffect } from "react";
-import marked from "marked";
-import "./App.css";
+{
+  /* <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p> */
+}
+import React, { useEffect, useState } from "react";
+import "../styles/App.css";
 
-const App = () => {
-  const [markdown, setMarkdown] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+function App() {
+  const [input, setInput] = useState("");
 
   useEffect(() => {
-    const fetchMarkdown = async () => {
-      setIsLoading(true);
-      // Simulate fetching markdown content from an API
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      const markdownContent = `# Markdown Example
-
-This is a **markdown** example.
-
-- List item 1
-- List item 2
-
-\`\`\`javascript
-console.log('Hello, Markdown!');
-\`\`\`
-      `;
-      setMarkdown(markdownContent);
-      setIsLoading(false);
-    };
-
-    fetchMarkdown();
+    return <h1>Loading...</h1>;
   }, []);
-
   return (
-    <div className="app">
-      <div className="editor">
-        <h2>Markdown Editor</h2>
+    <div>
+      <main className="app">
         <textarea
+          onChange={(e) => setInput(e.target.value)}
           className="textarea"
-          value={markdown}
-          onChange={(e) => setMarkdown(e.target.value)}
-        ></textarea>
-      </div>
-      <div className="preview">
-        <h2>Markdown Preview</h2>
-        {isLoading ? (
-          <p className="loading">Loading...</p>
-        ) : (
-          <div dangerouslySetInnerHTML={{ __html: marked(markdown) }} />
-        )}
-      </div>
+          placeholder="Text Area"
+        />
+        <section className="preview">
+          <h1>{input}</h1>
+        </section>
+      </main>
     </div>
   );
-};
+}
 
 export default App;
