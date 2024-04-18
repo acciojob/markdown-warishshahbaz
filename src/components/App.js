ca822dccc5dc4ea6b6fa09d37b09760fe4eb9a22
warishshahbaz <mdshahbazwarish1996@gmail.com>
2024-04-18 11:43:44 +0530

    init

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,90 @@
-import React, { useEffect, useState } from "react";
-import "../styles/App.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  Redirect,
+} from "react-router-dom";
+// react router dom version 5 was used here
 
-function App() {
-  const [input, setInput] = useState("");
-  const [isLoading, setIsLoading] = useState(true);
+import Login from "./Login";
+import Home from "./Home";
+import NotFound from "./NotFound";
 
-  useEffect(() => {
-    // simulate the loading process
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 3000);
-  }, []);
+// Custom PrivateRoute component
+const PrivateRoute = ({
+  component: Component,
+  isAuthenticated,
+  redirect,
+  ...rest
+}) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      isAuthenticated === true ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to={redirect} />
+      )
+    }
+  />
+);
 
-  if (isLoading) {
-    return <h1 className="loading">Loading...</h1>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+    };
+    this.login = this.login.bind(this);
   }
 
-  return (
-    <div>
-      <main className="app">
-        <textarea
-          onChange={(e) => setInput(e.target.value)}
-          className="textarea"
-          placeholder="Text Area"
-        />
-        <section className="preview">
-          <h1 id="Heading">{input}</h1>
-        </section>
-      </main>
-    </div>
-  );
+  login() {
+    this.setState({ isLoggedIn: !this.state.isLoggedIn });
+  }
+
+  render() {
+    const { isLoggedIn } = this.state;
+    return (
+      <Router>
+        <div className="main-container">
+          <div>
+            {isLoggedIn
+              ? "Logged in, Now you can enter Playground"
+              : "You are not authenticated, Please login first"}
+          </div>
+          <div>
+            <ul>
+              <li>
+                <Link to="/home">PlayGround</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </ul>
+          </div>
+          <Switch>
+            <Route
+              path="/login"
+              render={(props) => (
+                <Login {...props} login={this.login} isLogged={isLoggedIn} />
+              )}
+            />
+            <PrivateRoute
+              exact
+              path="/home"
+              component={Home}
+              isAuthenticated={isLoggedIn}
+              redirect="/login"
+            />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
